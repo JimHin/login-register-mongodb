da5ef74ee5b9e0ea68172457e0b00917e1aff9cd
JimKrow <krowstudio@icloud.com>
2022-04-13 17:40:03 +0200

    cryptage mot de passe et modèle user

--- src/interfaces/UserDocumentInterface.ts
+++ src/interfaces/UserDocumentInterface.ts
@@ -2,7 +2,11 @@ import mongoose from "mongoose";
 
 export interface UserDocument extends mongoose.Document {
     id: Number,
-    first_name: String,
-    last_name: String,
-    email: String
+    first_name: string,
+    last_name: string,
+    email: string,
+    password: string,
+    createdAt: Date,
+    updatedAt: Date,
+    comparePassword(candidatePassword: string): Promise<boolean>
 }
--- src/model/user.models.ts
+++ src/model/user.models.ts
@@ -1,17 +1,38 @@
 import mongoose from "mongoose";
-import { config } from "process";
 import bcrypt from "bcrypt"
+import { UserDocument } from "../interfaces/UserDocumentInterface";
+import config from 'config';
+
+
 
 const userSchema = new mongoose.Schema(
     {
         id: Number,
-        first_name: String,
-        last_name: String,
-        email: String
+        first_name: { type: String, required: true },
+        last_name: { type: String, required: true },
+        email: { type: String, required: true },
+        password: { type: String, required: true },
+        createdAt: { type: Date, required: true },
+        updatedAt: { type: Date, required: true },
     }
-
 );
 
+userSchema.pre("save", async function (next: any) {
+    let user = this as UserDocument;
+    if (!user.isModified("password")) {
+        return next();
+    }
+    bcrypt.genSalt(config.get("saltRounds"), function (err, salt) {
+        bcrypt.hash(user.password, salt, function (err, hash) {
+            user.password = hash;
+        });
+    });
+});
+
+userSchema.methods.comparePasswords = async function (candidatePassword: string | Buffer): Promise<boolean> {
+    let user = this as UserDocument;
+    return bcrypt.compare(candidatePassword, user.password).catch((e) => false);
+}
 
 const UserModel = mongoose.model("User", userSchema);
 export default UserModel;
